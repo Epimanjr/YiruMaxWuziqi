e9d5ac2ab6a3b5765543f68d66aa8ca2539dbaf1
TheMaster57 <maxime.blaise12@gmail.com>
2015-01-11 19:02:23 +0100

    Ajout des configurations de la fenêtre.

---
+++ src/graphique/Config.java
@@ -0,0 +1,39 @@
+/*
+ * Fichier de configuration.
+ */
+package graphique;
+
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author Maxime BLAISE
+ */
+public class Config {
+    
+    /**
+     * La taille du plateau. 
+     * Source : http://fr.wikipedia.org/wiki/Gomoku
+     */
+    public static int taillePlateau = 19;
+    
+    /**
+     * Largeur de la fenêtre.
+     */
+    public static int largeurFenetre = 650;
+    
+    /**
+     * Hauteur de la fenêtre.
+     */
+    public static int hauteurFenetre = 500;
+    
+    /**
+     * Titre de la fenêtre.
+     */
+    public static String titreFenetre = "YiruMaxWuziqi";
+    
+    /**
+     * Couleur de fond.
+     */
+    public static Color couleurFond = Color.rgb(255, 200, 250);
+}
